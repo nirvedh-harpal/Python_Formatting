6808988bfdc5d012f9e924ae5bcf0374c52b27b0
nirvedh-harpal <114853864+nirvedh-harpal@users.noreply.github.com>
2023-05-21 22:24:40 +0530

    Update a.py

--- a.py
+++ a.py
@@ -1,5 +1,18 @@
-def f():
-hello = 1
-          world = 3.0
-          follow = 2.0
-  return 0
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import re
+
+line = 'Cats are smarter than dogs'
+
+matchObj = re.match(r'dogs', line, re.M | re.I)
+if matchObj:
+    print 'match --> matchObj.group() : ', matchObj.group()
+else:
+    print 'No match!!'
+
+searchObj = re.search(r'dogs', line, re.M | re.I)
+if searchObj:
+    print 'search --> searchObj.group() : ', searchObj.group()
+else:
+    print 'Nothing found!!'
